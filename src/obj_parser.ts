59020c6b288fa6a6e2dfdc4d2c289f9f36362eeb
h80r <h80rgbc@gmail.com>
2024-05-30 14:24:52 -0300

    :bug: Include missing await on array mapping

--- src/obj_parser.ts
+++ src/obj_parser.ts
@@ -1,7 +1,7 @@
 import uploadImage from "./img_upload";
 
 const recursiveObjectParser = async <T>(input: T): Promise<T> => {
-  if (Array.isArray(input)) return input.map(recursiveObjectParser) as T;
+  if (Array.isArray(input)) return input.map(async e => await recursiveObjectParser(e)) as T;
   if (typeof input === 'object') for (const key in input) input[key] = await recursiveObjectParser(input[key]);
   if (typeof input === 'string') return (input.startsWith('data:image/') ? await uploadImage(input) : input) as T;
   return input;
